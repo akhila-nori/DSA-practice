921ed25e7d783ca2009f46c70f7795616f7fcfb8
akhila-nori <akhilanori2001@gmail.com>
2025-12-01 15:45:03 +0530

    important code comments added

--- ExceptionHandling/src/Main.java
+++ ExceptionHandling/src/Main.java
@@ -13,8 +13,18 @@ public class Main {
             System.out.println(a[8]);
 
         } catch(ArrayIndexOutOfBoundsException | ArithmeticException e){
-            System.out.println("Handling the exception .... "+e.getStackTrace());
-            System.out.println("e is ..."+e.toString());
+            System.out.println("Handling the exception .... "+e.getStackTrace()+"\n");
+
+
+//            System.out.println("Handling the exception .... "+e.printStackTrace()); --> Error line
+
+            //why Reasoning :-
+            // e.printStackTrace() --> this completes task but gives no value back --> Void (No value retrned
+            // Here you are asking the printStackTrace() to deliver the stack trace (message) to the console
+            // the return type to be delivered to
+            // Action 2: Execute the stack trace printing (Void)
+
+            e.printStackTrace();// This is an action, not a value. It's valid on its own line.
         }
 
         System.out.println("Bye guys ");
