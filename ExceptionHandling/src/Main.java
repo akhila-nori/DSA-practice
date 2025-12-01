43f5545e7fd124e20617044f1e26f8ca11c38167
akhila-nori <akhilanori2001@gmail.com>
2025-12-01 15:03:09 +0530

    try-catch block added - so that the below lines are also executed

--- ExceptionHandling/src/Main.java
+++ ExceptionHandling/src/Main.java
@@ -2,7 +2,19 @@
 public class Main {
     public static void main(String[] args) {
          int a[] = new int[5];
-        System.out.println(a[0]);
+
+
         System.out.println("Hello guys ");
+
+        try{
+            System.out.println(a[8]);
+        } catch(ArrayIndexOutOfBoundsException e){
+            System.out.println("Tried to access the out of bound element");
+        }
+
+        System.out.println("Bye guys ");
+
+
+
          }
     }
