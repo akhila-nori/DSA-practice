c189f43d1217c6109b008d5744d81edfaba96e1c
akhila-nori <akhilanori2001@gmail.com>
2025-12-01 15:09:24 +0530

    multiple catch blocks

--- ExceptionHandling/src/Main.java
+++ ExceptionHandling/src/Main.java
@@ -7,14 +7,19 @@ public class Main {
         System.out.println("Hello guys ");
 
         try{
+
+            int result = 5/0;
+
             System.out.println(a[8]);
+
         } catch(ArrayIndexOutOfBoundsException e){
-            System.out.println("Tried to access the out of bound element");
+            System.out.println("Tried to access the out of bound element"+e.getStackTrace());
+            System.out.println("e is ..."+e.toString());
+        } catch(ArithmeticException e){
+            System.out.println("arithmetic exception ...."+e.getMessage());
         }
 
         System.out.println("Bye guys ");
-
-
-
+        
          }
     }
