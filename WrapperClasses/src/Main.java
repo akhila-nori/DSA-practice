af62a902a6d28bf56c194f6d9a58925629283a4f
akhila-nori <akhilanori2001@gmail.com>
2025-12-01 20:50:09 +0530

    Wrapper class - primitive data type

--- WrapperClasses/src/Main.java
+++ WrapperClasses/src/Main.java
@@ -1,12 +1,14 @@
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
 
-        //converting primitive datatype (12) to Integer WrapperClass to be used in Collection Framework
-        Integer obj =  Integer.valueOf(12); //returning new Integer
+        Integer a = Integer.valueOf("12");
+        System.out.println("a is ..."+a);
+        String dataType = a.getClass().getName();
 
-        int age = obj; //unboxing
+        System.out.println("a's data type is: " + dataType);
 
 
 
