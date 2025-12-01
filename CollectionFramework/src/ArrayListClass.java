ee34d88debbaf0095ee5a24fb56091d546ba399d
akhila-nori <akhilanori2001@gmail.com>
2025-12-01 22:33:13 +0530

    ArrayList class addAll()

--- CollectionFramework/src/ArrayListClass.java
+++ CollectionFramework/src/ArrayListClass.java
@@ -6,20 +6,22 @@ public class ArrayListClass {
 
         List<Integer> list = new ArrayList<>();
 
+        List<Integer> list2 = new ArrayList<>();
+
         list.add(10);
         list.add(20);
         list.add(30);
-        System.out.println(list);
-        System.out.println(list.size());
-        System.out.println(list.contains(20));
 
-        list.remove(Integer.valueOf(1)); //returns Integer object 1 (here object is returned)
-         //takes object and removes the object , it creates an INTEGER OBJECT with the value 1
 
-        //The remove(Object o) method searches your list ([10, 20, 30]) for an element that is equal to the number 1.
-        //Since the number 1 is not in your list, the remove() method does nothing and returns false.
+        list2.add(1);
+        list2.add(2);
+        list2.add(3);
 
-        System.out.println(list);
+
+        System.out.println(list2);
+
+        list2.addAll(list);
+        System.out.println(list2);
 
 
     }
