f6b5f80f018ca633a0ec7cd121289e0807910031
akhila-nori <akhilanori2001@gmail.com>
2025-12-12 13:27:22 +0530

    Priority Queue implementation for String descending order

--- CollectionFramework/Queue/src/LearnPriorityQueue.java
+++ CollectionFramework/Queue/src/LearnPriorityQueue.java
@@ -3,17 +3,30 @@ import java.util.Queue;
 
 public class LearnPriorityQueue {
     public static void main(String[] args) {
-        Queue<Integer> pq = new PriorityQueue<>((a,b) -> b-a);
+//        Queue<Integer> pq = new PriorityQueue<>((a,b) -> b-a);
 
-        pq.add(30);
-        pq.add(40);
-        pq.add(20);
-        pq.add(10);
+        //descending order
+        PriorityQueue<String> dictionary = new PriorityQueue<>((a,b)-> b.compareToIgnoreCase(a));
+        dictionary.add("Akhila");
+        dictionary.add("ABC");
+        dictionary.add("Anila");
+        dictionary.add("DEF");
+        dictionary.add("GHI");
 
-        System.out.println(pq);
-        System.out.println(pq.poll());
-        System.out.println(pq);
-        System.out.println(pq.poll());
+        System.out.println(dictionary);
+        System.out.println(dictionary.poll());
+        System.out.println(dictionary);
+
+
+//        pq.add(30);
+//        pq.add(40);
+//        pq.add(20);
+//        pq.add(10);
+//
+//        System.out.println(pq);
+//        System.out.println(pq.poll());
+//        System.out.println(pq);
+//        System.out.println(pq.poll());
 
     }
 }
