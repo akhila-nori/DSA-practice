b4883e1b0fb5207a3b28f38f6d9f4b6a186e6d97
akhila-nori <akhilanori2001@gmail.com>
2025-12-12 14:16:53 +0530

    Set interface impelmentation using LinkedHashSet, TreeSet (implements binary search tree) -> time complexity

--- CollectionFramework/Sets/src/Main.java
+++ CollectionFramework/Sets/src/Main.java
@@ -1,16 +1,23 @@
 import java.util.HashSet;
+import java.util.LinkedHashSet;
 import java.util.Set;
+import java.util.TreeSet;
 
 
 public class Main {
     public static void main(String[] args) {
 
-        Set<Integer> set = new HashSet<>();
-        set.add(10);
-        set.add(10);
-        set.add(10);
+//        Set<Integer> set = new HashSet<>();    //O(1) time complexity
+//        Set<Integer> set = new LinkedHashSet<>();  //O(n) time complexity
+        Set<Integer> set = new TreeSet<>(); // O(log n) time complexity
         set.add(10);
         set.add(20);
+        set.add(30);
+        set.add(5);
+        System.out.println(set);
+
+        set.remove(10);
+        System.out.println(set.contains(10));;
         System.out.println(set);
 
         }
